57b5c6d81ff1e5be899cee93a82f45ff9d355d25
qskane <qskane@qq.com>
2018-09-14 22:14:50 +0800

    Update UI

--- resources/views/components/rows/products.blade.php
+++ resources/views/components/rows/products.blade.php
@@ -1,5 +1,22 @@
-<div class="row products-container">
-    @foreach($products as $product)
-        @include('components.cols.product')
-    @endforeach
+{{--<div class="products-container">--}}
+{{--@foreach(array_chunk($products->toArray(),4) as  $productChunk)--}}
+
+{{--<div class="mdl-grid">--}}
+{{--@foreach($productChunk as $product)--}}
+{{--<div class="mdl-cell mdl-cell--4-col">--}}
+{{--                    @include('components.cols.product')--}}
+{{--</div>--}}
+{{--@endforeach--}}
+{{--</div>--}}
+
+{{--@endforeach--}}
+{{--</div>--}}
+
+<div class="mdl-grid">
+    <div class="mdl-cell mdl-cell--3-col"  style="background: darkgrey;">3</div>
+    <div class="mdl-cell mdl-cell--3-col"  style="background: darkgrey;">3</div>
+    <div class="mdl-cell mdl-cell--3-col"  style="background: darkgrey;">3</div>
+    <div class="mdl-cell mdl-cell--3-col"  style="background: darkgrey;">3</div>
 </div>
+
+
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,9 @@
 @extends("layouts.app")
 @section('header')
-    @include('home._slider')
+{{--    @include('home._slider')--}}
 @endsection
 @section('content')
     @include('components.rows.products')
 @endsection
+
+
