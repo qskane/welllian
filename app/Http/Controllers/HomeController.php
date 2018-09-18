e324d5b4cf889dbf4057346b1d6ba7033bde4713
qskane <qskane@qq.com>
2018-09-18 22:26:53 +0800

    Update doces

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,4 +15,14 @@ class HomeController extends Controller
         return view('home.index', compact('products'));
     }
 
+    public function test()
+    {
+        $url = 'https://oauth.jd.com/oauth/authorize?response_type=code&client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&state=STATE_CODE';
+
+        $url = str_replace(['CLIENT_ID', 'REDIRECT_URI', 'STATE_CODE'], ['2EBCF76F88CD64790F0D22478BB60399', 'http://malllian-dev.com', '1234'], $url);
+
+        echo $url;
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 
 Route::get('/', "HomeController@index")->name('home');
+Route::get('/test', "HomeController@test")->name('test');
 Auth::routes(['verify' => true]);
 
 Route::resource('product', 'ProductController');
